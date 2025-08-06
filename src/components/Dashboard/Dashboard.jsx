db8d8969d6a9f2ffb9a991eea18b83c35c7f460a
kutah1 <jameskutah47@gmail.com>
2025-08-06 12:25:19 +0100

    in progress

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 import { motion } from 'framer-motion';
+import { FaArrowsRotate } from "react-icons/fa6";
 import { FaBars, FaUser, FaEnvelope, FaMoneyBill, FaStar, FaUpload, FaCog, FaSearch, FaGem, FaUserGraduate, FaTools, FaQuestionCircle } from 'react-icons/fa';
 import { HiOutlineBell, HiOutlineCog } from 'react-icons/hi';
+import { FiHelpCircle } from "react-icons/fi";
+import { FaLocationCrosshairs } from "react-icons/fa6";
+import { FaUserCircle } from "react-icons/fa";
 import { BsCardList } from 'react-icons/bs';
 import { FiUser } from 'react-icons/fi';
 import { GiBrickWall } from "react-icons/gi";
@@ -15,11 +19,12 @@ import LadyInPink from '../../assets/images/DashboardImages/LadyInPink.jpg'
 import Screen from '../../assets/images/DashboardImages/Screen.jpg'
 
 const dummyCards = [
-  { id: 1, title: 'SEO Blog Writing', rating: 5, image: Screen, talent: 'Talent A' },
-  { id: 2, title: 'Photo Retouching', rating: 4, image: LadyInPink, talent: 'Talent B' },
-  { id: 3, title: 'SEO Blog Writing', rating: 5, image: Screen, talent: 'Talent C' },
-  { id: 4, title: 'Brand Design', rating: 4, image: LadyInPink, talent: 'Talent D' },
-  { id: 5, title: 'UX Review', rating: 4, image:Screen, talent: 'Talent E' },
+  { id: 1, title: 'SEO Blog Writing',description:`Boost your website traffic with  engaging, SEO-optimized blog posts crafted to drive results and keep your  readers hooked` ,
+    rating: 5, image: Screen, talent: 'Talent A' },
+  { id: 2, title: 'Photo Retouching',description:`Bring your photos to life with expert  color correction, skin retouching, and  detailed enhancements for a polished finish`, rating: 4, image: LadyInPink, talent: 'Talent B' },
+  { id: 3, title: 'SEO Blog Writing',description:'Boost your website traffic with engaging, SEO-optimized blog posts crafted to drive results and keep your readers hooked', rating: 5, image: Screen, talent: 'Talent C' },
+  { id: 4, title: 'Brand Design', rating: 4,description:'Boost your website traffic with engaging, SEO-optimized blog posts crafted to drive results and keep your readers hooked', image: LadyInPink, talent: 'Talent D' },
+  { id: 5, title: 'UX Review', rating: 4,description:'Boost your website traffic with engaging, SEO-optimized blog posts crafted to drive results and keep your readers hooked', image:Screen, talent: 'Talent E' },
 ];
 
 const SidebarIcon = ({ icon, label }) => (
@@ -38,16 +43,16 @@ const Dashboard = () => {
       {/* Sidebar */}
       <motion.div
         animate={{ width: expanded ? 220 : 80 }}
-        className="bg-orange-600 text-white p-4 pt-6 group transition-all duration-300"
+        className="bg-orange-600 text-white p-4 pt-6 group transition-all duration-100"
         onMouseEnter={() => setExpanded(true)}
         onMouseLeave={() => setExpanded(false)}
       >
-        <SidebarIcon icon={<GiBrickWall />} label="Dashboard" className="w-7 h-7 " />
-        <SidebarIcon icon={<BsChatTextFill  className="w-7 h-7 text-[#0F172A]" />} label="Messages" />
-        <SidebarIcon icon={<GiTakeMyMoney className="w-7 h-7 text-[#0F172A]   " />} label="Payment" />
-        <SidebarIcon icon={<TfiSharethisAlt  className="w-7 h-7 text-[#0F172A]" />} label="Reviews" />
-        <SidebarIcon icon={<BiSolidSend  className="w-7 h-7 text-[#0F172A]" />} label="Submission" />
-        <SidebarIcon icon={<CgProfile  className="w-7 h-7 text-[#0F172A]" />} label="Account" />
+        <SidebarIcon icon={<GiBrickWall />} label="Dashboard" className="w-7 h-7 my-5 mt-50 " />
+        <SidebarIcon icon={<BsChatTextFill  className="w-7 h-7 text-[#0F172A]  my-5" />} label="Messages" />
+        <SidebarIcon icon={<GiTakeMyMoney className="w-7 h-7 text-[#0F172A]   my-5  " />} label="Payment" />
+        <SidebarIcon icon={<TfiSharethisAlt  className="w-7 h-7 text-[#0F172A]  my-5" />} label="Reviews" />
+        <SidebarIcon icon={<BiSolidSend  className="w-7 h-7 text-[#0F172A]  my-5" />} label="Submission" />
+        <SidebarIcon icon={<FaUserCircle className="w-7 h-7 text-[#0F172A]  my-5" />} label="Account" />
       </motion.div>
 
       {/* Main content */}
@@ -56,7 +61,7 @@ const Dashboard = () => {
         <div className="flex justify-between items-center mb-6">
           <div className="flex gap-2 items-center">
             <input className="border px-4 py-2 rounded-full" placeholder="Search for talents" />
-            <button className="bg-black text-white px-4 py-2 rounded-full">Search</button>
+            <button className="bg-[#0F172A] text-white px-4 py-2 rounded-full">Search</button>
             <button className="border px-4 py-2 rounded-full">Talent gig</button>
             <button className="border px-4 py-2 rounded-full">Lecture-featured gig</button>
           </div>
@@ -69,38 +74,42 @@ const Dashboard = () => {
         {/* Profile, Upgrade & Orders */}
         <div className="flex-row flex  gap-6 mb-6">
           {/* Profile */}
-          <div className="bg-orange-500 p-4 rounded-xl shadow flex-row flex justify-center items-center gap-4 flex-1">
-            
-            <div className='flex-col text-center self-center justify-center'>
+          <div className="bg-orange-500 p-4 rounded-xl shadow flex-row flex justify-center items-center gap-4 flex-1 ">
+            <div className='flex-col text-center self-center justify-center w-1/3'>
               <img src={AvatarInBlue} className="rounded-full w-15 h-15 object-center self-center ml-13" alt="profile" />
-
               <p className="font-semibold text-white">Hello Kojo 👋</p>
               <div className="flex gap-2 mt-2 bg-white px-6 rounded-3xl p-2">
-                <button className="text-black px-2 py-1 rounded-md text-sm">Client</button>
-                <button className="text-black px-2 py-1 rounded-md text-sm">Kumasi</button>
+                <button className="text-[#0F172A] px-2 py-1 rounded-md text-sm flex flex-row items-center transform -translate-x-3">
+                  <FaUserCircle className='text-xl' />
+                  
+                  <span className='mx-1'>
+                    Client
+                    </span></button>
+                <button className="text-[#0F172A] px-2 py-1 rounded-md text-sm flex flex-row items-center transform -translate-x-3">
+                  <FaLocationCrosshairs className='text-xl' />
+                  <span className='mx-1'>Kumasi
+                    </span> </button>
               </div>
             </div>
           </div>
 
           {/* Upgrade & Track Orders */}
           <div className="flex flex-col gap-6 flex-1">
-            <div className="bg-black text-white p-12 rounded-xl shadow">
+            <div className="bg-[#0F172A] text-white p-12 rounded-xl shadow">
               <div className="flex items-center justify-between gap-4">
                 <div>
-                  <h2 className="font-semibold mb-1">Upgrade to Pro</h2>
-                  <p className="text-sm">Unlock featured student matching</p>
+                  <h2 className="font-semibold mb-1 text-2xl">Upgrade to Pro</h2>
+                  <p className="">Unlock featured student <br /> matching</p>
                 </div>
-
-                 <FaGem size={38} className="text-orange-400 items-end" />
-
+                <FaGem size={38} className="text-orange-400 items-end" />
               </div>
             </div>
             <div className="bg-white p-4 rounded-xl shadow flex items-start gap-4">
-              <BsCardList size={32} className="text-orange-500" />
+              <FaArrowsRotate size={32}  />
               <div>
                 <h2 className="font-semibold">Track Orders</h2>
-                <p className="text-sm text-gray-600">Monitor your ongoing gigs in real-time</p>
-                <button className="bg-orange-500 px-10 p-1 rounded-xl text-white  mt-1">Visit now</button>
+                <p className="text-sm text-[#0F172A]">Monitor your ongoing gigs in real-time</p>
+                <button className="bg-orange-500 px-15 p-2 mt-5 rounded-xl text-white  ">Visit now</button>
               </div>
             </div>
           </div>
@@ -108,27 +117,25 @@ const Dashboard = () => {
 
         {/* Info cards */}
         <div className="grid grid-cols-3 gap-6 mb-6">
-          <div className="bg-white p-4 rounded-xl shadow flex items-start gap-4">
-            <FaUserGraduate size={28} className="text-orange-500" />
+          <div className="bg-white p-4 rounded-xl shadow flex flex-row items-start  gap-4">
+            <FaUserGraduate size={28}  className='text-xl'/>
             <div>
-              <p className="mb-2 text-sm">Are you a student looking to offer services on GigUp?</p>
-              <button className="bg-orange-500 text-white px-4 py-2 rounded-md text-sm">Verify as a student</button>
+              <p className="mb-2 text-sm">Are you a student looking to offer  <br />services on GigUp?</p>
+              <button className="bg-orange-500 text-white px-4 py-2 rounded-2xl p-2 mt-4 text-sm">Verify as a student</button>
             </div>
           </div>
-
           <div className="bg-white p-4 rounded-xl shadow flex items-start gap-4">
-            <FaTools size={28} className="text-orange-500" />
+            < FaUserCircle size={28} className='text-xl'  />
             <div>
               <h2 className="font-semibold mb-1">Account & Profile Settings</h2>
-              <p className="text-sm text-gray-600">Edit your profile and manage your account settings easily.</p>
+              <p className="text-sm text-[#0F172A]">Edit your profile and manage your <br />account settings easily.</p>
             </div>
           </div>
-
           <div className="bg-white p-4 rounded-xl shadow flex items-start gap-4">
-            <FaQuestionCircle size={28} className="text-orange-500" />
+            <FiHelpCircle  size={28}  className='text-xl' />
             <div>
               <h2 className="font-semibold mb-1">Help / Support</h2>
-              <p className="text-sm text-gray-600">Get assistance or find answers to your questions.</p>
+              <p className="text-sm text-[#0F172A]">Get assistance or find answers to <br /> your questions.</p>
             </div>
           </div>
         </div>
@@ -139,17 +146,26 @@ const Dashboard = () => {
             <h2 className="font-bold text-xl">Recently viewed gig</h2>
             <button className="text-orange-500" onClick={() => setShowAll(true)}>See all</button>
           </div>
+
+          {/* Updated slider section */}
           <div className="grid grid-cols-3 gap-4">
             {(showAll ? dummyCards : dummyCards.slice(0, 3)).map((card) => (
               <motion.div
                 key={card.id}
                 whileHover={{ scale: 1.03 }}
-                className="bg-white p-4 rounded-xl shadow cursor-pointer"
+                className="bg-white rounded-xl shadow cursor-pointer flex flex-col justify-center p-4 h-92"
               >
-                <img src={card.image} alt={card.title} className="w-full h-40 object-cover rounded-md mb-3" />
-                <p className="font-semibold">{card.title}</p>
-                <p className="text-sm text-gray-600">by {card.talent}</p>
-                <p className="text-orange-500 mt-1">Pro</p>
+                <img
+                  src={card.image}
+                  alt={card.title}
+                  className="w-full h-40 object-cover rounded-md mb-3"
+                />
+                <p className="font-semibold">{card.description}</p>
+                <div className='flex flex-row justify-between mt-4 items-center'>
+                   <p className="text-sm  text-orange-500 "> {card.talent}</p>
+                <p className="text-white bg-orange-500 rounded-xl px-6 p-1 mt-1 mr-5">Pro</p>
+                </div>
+               
                 <div className="flex mt-1">
                   {'★'.repeat(card.rating)}{'☆'.repeat(5 - card.rating)}
                 </div>
